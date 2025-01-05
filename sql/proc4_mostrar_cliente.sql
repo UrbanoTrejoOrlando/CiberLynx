c2bb6c5f219b1e596213e63ba9878a3d47b50ced
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 12:59:20 -0600

    Update proc4_mostrar_cliente.sql
    Procedure para mostrar un cliente

--- sql/proc4_mostrar_cliente.sql
+++ sql/proc4_mostrar_cliente.sql
@@ -1,1 +1,11 @@
+DROP PROCEDURE IF EXISTS proc_mostrar_clientes_nuevo;
 
+DELIMITER //
+
+CREATE PROCEDURE proc_mostrar_clientes_nuevo()
+BEGIN
+SELECT 
+Clv_Cliente, Nombre, Apellido_Paterno, Apellido_Materno FROM Cliente WHERE Estatus = '1';
+END //
+
+DELIMITER ;

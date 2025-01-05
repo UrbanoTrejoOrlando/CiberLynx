c782f6ecf9b54f53042626092584d47fe6468550
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 17:08:09 -0600

    Update proc13_eliminar_marca.sql
    Procedure para eliminar una marca

--- sql/proc13_eliminar_marca.sql
+++ sql/proc13_eliminar_marca.sql
@@ -1,1 +1,11 @@
+DROP PROCEDURE IF EXISTS proc_eliminar_marca;
 
+DELIMITER //
+CREATE PROCEDURE proc_eliminar_marca(
+        IN p_id_marca INT
+)
+BEGIN
+        UPDATE Marca SET Estatus = '0'
+                WHERE Id_Marca = p_id_Marca;
+END //
+DELIMITER ;

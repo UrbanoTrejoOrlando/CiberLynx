1a9ec3ceb36e0159c65534987492faba43c6018c
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 16:58:51 -0600

    Update proc6_actualizar_cliente.sql
    Procedure para actualizar informacion de un cliente

--- sql/proc6_actualizar_cliente.sql
+++ sql/proc6_actualizar_cliente.sql
@@ -1,1 +1,18 @@
+DROP PROCEDURE IF EXISTS proc_actualizar_cliente;
 
+DELIMITER //
+
+CREATE PROCEDURE proc_actualizar_cliente(
+IN p_nombre VARCHAR(20),
+IN p_apellidop VARCHAR(20),
+IN p_apellidom VARCHAR(20)
+)
+BEGIN 
+UPDATE Cliente SET
+Nombre = p_nombre,
+Apellido_Paterno = p_apellidop,
+Apellido_Materno = p_apellidom
+WHERE Nombre = p_nombre;
+END //
+
+DELIMITER ;

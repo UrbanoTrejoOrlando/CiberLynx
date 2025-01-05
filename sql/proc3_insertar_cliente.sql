7e3eb358060593176324bb60753918429e370dec
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 12:58:12 -0600

    Update proc3_insertar_cliente.sql
    procedimiento para insertar un cliente

--- sql/proc3_insertar_cliente.sql
+++ sql/proc3_insertar_cliente.sql
@@ -1,1 +1,14 @@
+DELIMITER ;;
+CREATE DEFINER=`Orlando`@`%` PROCEDURE `proc_insertar_cliente`(
+    IN p_nombre VARCHAR(20),
+    IN p_apellidop VARCHAR(20),
+    IN p_apellidom VARCHAR(20)
+)
+BEGIN
+    DECLARE p_clv_cliente VARCHAR(5);
+    SET p_clv_cliente = GenerarClaveAleatoria12();
 
+        INSERT INTO Cliente (Clv_Cliente, Nombre, Apellido_Paterno, Apellido_Materno)
+        VALUES (p_clv_cliente, p_nombre, p_apellidop, p_apellidom);
+END ;;
+DELIMITER ;

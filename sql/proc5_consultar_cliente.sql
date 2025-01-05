d3552beb7c99552c91f435990e55b6a4db9eeae4
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 13:00:43 -0600

    Update proc5_consultar_cliente.sql
    Procedure para consultar un cliente

--- sql/proc5_consultar_cliente.sql
+++ sql/proc5_consultar_cliente.sql
@@ -1,1 +1,13 @@
+DROP PROCEDURE IF EXISTS proc_consultar_cliente;
 
+DELIMITER //
+
+CREATE PROCEDURE proc_consultar_cliente(
+IN p_clv_cliente VARCHAR(5)
+)
+BEGIN 
+SELECT Nombre, Apellido_Paterno, Apellido_Materno
+FROM Cliente WHERE Clv_Cliente = p_clv_cliente;
+END //
+
+DELIMITER ;

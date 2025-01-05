598297cf6f0edd610fef558f0eab7d6e6440bf5c
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 12:54:42 -0600

    Update proc_1_inserta_sesion.sql
    Procedimiento para insertar una sesion

--- sql/proc_1_inserta_sesion.sql
+++ sql/proc_1_inserta_sesion.sql
@@ -1,1 +1,16 @@
 
+DELIMITER //
+
+CREATE PROCEDURE proc_inserta_sesion(
+    IN p_clv_usuario VARCHAR(5)
+)
+BEGIN
+    DECLARE p_folio_sesion VARCHAR(12); -- Ajustado para 12 caracteres
+    SET p_folio_sesion = GenerarClaveAleatoria12();
+
+    -- Inserción en la tabla Sesion
+    INSERT INTO Sesion (Folio_Sesion, Clv_Usuario) 
+    VALUES (p_folio_sesion, p_clv_usuario);
+END //
+
+DELIMITER ;

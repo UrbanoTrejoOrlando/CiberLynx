5db964b481dddc9060591846a0b90b14d456fcf0
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 17:04:17 -0600

    Update proc10_insertar_unidad.sql
    Procedure para insertar una unidad

--- sql/proc10_insertar_unidad.sql
+++ sql/proc10_insertar_unidad.sql
@@ -1,1 +1,10 @@
+DROP PROCEDURE IF EXISTS proc_insertar_unidad;
 
+DELIMITER //
+CREATE PROCEDURE proc_insertar_unidad(
+        IN p_nombre VARCHAR(20)
+)
+BEGIN
+        INSERT INTO Unidad (Nombre_Unidad) VALUES (p_nombre);
+END //
+DELIMITER ;

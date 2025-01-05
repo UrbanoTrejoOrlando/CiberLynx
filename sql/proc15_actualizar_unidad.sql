9643971d1f89180faa2d915d1b8ad8c52020d4cd
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 17:12:24 -0600

    Update proc15_actualizar_unidad.sql
    Procedure para actualizar unidad

--- sql/proc15_actualizar_unidad.sql
+++ sql/proc15_actualizar_unidad.sql
@@ -1,1 +1,14 @@
 
+DROP PROCEDURE IF EXISTS proc_actualizar_unidad;
+
+DELIMITER //
+CREATE PROCEDURE proc_actualizar_unidad(
+   IN p_id_unidad INT,
+   IN p_nombre_unidad VARCHAR(20)
+)
+BEGIN 
+   UPDATE Unidad SET 
+   Nombre_Unidad = p_nombre_unidad
+   WHERE Id_Unidad = p_id_unidad;
+END //
+DELIMITER ;

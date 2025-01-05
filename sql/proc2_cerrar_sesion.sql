a7539debc0e805a037a3a6d82e32ff82bf458841
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 12:56:40 -0600

    Update proc2_cerrar_sesion.sql
    Procedure para cerrar una sesion

--- sql/proc2_cerrar_sesion.sql
+++ sql/proc2_cerrar_sesion.sql
@@ -1,1 +1,13 @@
 
+DROP PROCEDURE IF EXISTS proc_cerrar_sesion;
+
+DELIMITER //
+
+CREATE PROCEDURE proc_cerrar_sesion(
+    IN p_clv_usuario VARCHAR(5)
+)
+BEGIN
+    UPDATE Sesion SET Estatus = '0' WHERE Clv_Usuario = p_clv_usuario;
+END //
+
+DELIMITER ;

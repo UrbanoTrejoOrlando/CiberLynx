7b6382c561a49553ed913af248a76a795f73330a
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 17:05:31 -0600

    Update proc11_obtener_marca.sql
    Procedure para obtener una nueva marca

--- sql/proc11_obtener_marca.sql
+++ sql/proc11_obtener_marca.sql
@@ -1,1 +1,12 @@
+DROP PROCEDURE IF EXISTS proc_marca;
 
+DELIMITER //
+
+CREATE PROCEDURE proc_marca(
+    IN p_id_marca INT
+)
+BEGIN 
+SELECT Id_Marca, Nombre_Marca FROM Marca WHERE Id_Marca = p_id_marca;
+END //
+
+DELIMITER ;

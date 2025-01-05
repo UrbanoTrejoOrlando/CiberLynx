07c181e8d71d26f8dbae0b0c56a76c37acac3faf
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 17:09:08 -0600

    Update proc14_consultar_unidad.sql
    Procedure para consultar una unidad

--- sql/proc14_consultar_unidad.sql
+++ sql/proc14_consultar_unidad.sql
@@ -1,1 +1,10 @@
+DROP PROCEDURE IF EXISTS proc_unidad;
 
+DELIMITER //
+CREATE PROCEDURE proc_unidad(
+    IN p_id_unidad INT
+)
+BEGIN 
+SELECT Id_Unidad, Nombre_Unidad FROM Unidad WHERE Id_Unidad = p_id_unidad;
+END //
+DELIMITER ;

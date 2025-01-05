043b93671dafa0226b09261dc05c0c81c93e5f3d
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 17:02:34 -0600

    Update proc9_insertar_marca.sql
    Procedure para insertar una marca

--- sql/proc9_insertar_marca.sql
+++ sql/proc9_insertar_marca.sql
@@ -1,1 +1,10 @@
+DROP PROCEDURE IF EXISTS proc_insertar_marca;
 
+DELIMITER //
+CREATE PROCEDURE proc_insertar_marca(
+        IN p_nombre VARCHAR(20)
+)
+BEGIN
+        INSERT INTO Marca (Nombre_Marca) VALUES (p_nombre);
+END //
+DELIMITER ;

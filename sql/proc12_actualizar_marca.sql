d6675ccb6a8f91cd2f8c162db3bcaf6b2c6016cf
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-05 17:06:34 -0600

    Update proc12_actualizar_marca.sql
    Procedure para actualizar marca

--- sql/proc12_actualizar_marca.sql
+++ sql/proc12_actualizar_marca.sql
@@ -1,1 +1,15 @@
+DROP PROCEDURE IF EXISTS proc_actualizar_marca;
 
+DELIMITER //
+
+CREATE PROCEDURE proc_actualizar_marca(
+   IN p_id_Marca INT,
+   IN p_nombre_marca VARCHAR(20)
+)
+BEGIN 
+   UPDATE Marca SET 
+   Nombre_Marca = p_nombre_marca
+   WHERE Id_Marca = p_id_Marca;
+END //
+
+DELIMITER ;

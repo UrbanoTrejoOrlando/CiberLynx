c4a93c1cdf7a86a76b18ab03343755053a70ff04
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-06 19:47:58 -0600

    Update proc25_mostrar_usuario.sql
    Procedure para mostrar un usuario

--- sql/proc25_mostrar_usuario.sql
+++ sql/proc25_mostrar_usuario.sql
@@ -1,1 +1,17 @@
+DROP PROCEDURE IF EXISTS proc_usuario_2;
+DELIMITER //
 
+CREATE PROCEDURE proc_usuario_2()
+BEGIN
+    SELECT
+    Clv_Usuario, 
+    Rol.Nombre_Rol,
+    Nombre,
+    Apellido_Paterno,
+    Apellido_Materno,
+    Usuario_Nombre,
+    Contrasenia FROM Usuario
+    INNER JOIN Rol ON Usuario.Id_Rol = Rol.Id_Rol
+    WHERE Usuario.Estatus = '1';
+END //
+DELIMITER ;

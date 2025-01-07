c5d2a54a89b87f7d7d9d1647b94ae17a086979ae
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-06 19:45:28 -0600

    Update proc23_actualizar_inventario.sql
    Procedure para actualizar el inventario al realizar una compra

--- sql/proc23_actualizar_inventario.sql
+++ sql/proc23_actualizar_inventario.sql
@@ -1,1 +1,23 @@
-
+DROP PROCEDURE IF EXISTS proc_actualizar_inventario;
+DELIMITER //
+CREATE PROCEDURE proc_actualizar_inventario(
+    IN p_nombre VARCHAR(30),
+    IN p_descripcion VARCHAR(40),
+    IN p_precio DOUBLE,
+    IN p_existencia INT,
+    IN p_marca INT,
+    IN p_categoria INT,
+    IN p_unidad INT
+)
+BEGIN
+    UPDATE Producto SET
+        Nombre = p_nombre,
+        Descripcion = p_descripcion,
+        Precio = p_precio,
+        Existencia = p_existencia,
+        Id_Marca = p_marca,
+        Id_Categoria = p_categoria,
+        Id_Unidad = p_unidad
+    WHERE Nombre = p_nombre;
+END //
+DELIMITER ;

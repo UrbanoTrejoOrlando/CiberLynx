a5696faddfb2e40721e70f84312a60b9683b93bf
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-06 19:31:59 -0600

    Update proc17_mostrar_inventario.sql
    Procedure para mostrar el inventario existente

--- sql/proc17_mostrar_inventario.sql
+++ sql/proc17_mostrar_inventario.sql
@@ -1,1 +1,21 @@
 
+DROP PROCEDURE IF EXISTS proc_mostrar_inventario;
+
+DELIMITER //
+CREATE PROCEDURE proc_mostrar_inventario()
+BEGIN
+SELECT 
+Codigo_Barras, Nombre, Descripcion, Precio,
+Nombre_Marca, Nombre_Categoria, Nombre_Unidad,
+CASE Producto.Estatus
+WHEN 1 THEN 'Activo'
+ELSE 'Inactivo'
+END AS 'Estatus' ,
+Existencia
+FROM Producto
+INNER JOIN Marca ON Producto.Id_Marca = Marca.Id_Marca
+INNER JOIN Categoria ON Categoria.Id_Categoria = Producto.Id_Categoria
+INNER JOIN Unidad ON Unidad.Id_Unidad = Producto.Id_Unidad
+WHERE Producto.Estatus = '1';
+END //
+DELIMITER ;

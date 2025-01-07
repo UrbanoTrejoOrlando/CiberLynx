cae7b416e78185db05a19d7644c28f7945b3054b
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-06 19:30:01 -0600

    Update proc16_eliminar_unidad.sql
    Procedure para eliminar una unidad

--- sql/proc16_eliminar_unidad.sql
+++ sql/proc16_eliminar_unidad.sql
@@ -1,1 +1,11 @@
+CREATE PROCEDURE IF EXISTS proc_eliminar_unidad;
 
+DELIMITER //
+CREATE PROCEDURE proc_eliminar_unidad(
+        IN p_id_unidad INT
+)
+BEGIN 
+    UPDATE Unidad SET Estatus = '0'
+        WHERE Id_Unidad = p_id_unidad;
+END //
+DELIMITER ;

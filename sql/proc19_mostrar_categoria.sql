d11cc6a93ff7b719d6da3ffba18b732e8e7c7463
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-06 19:34:31 -0600

    Update proc19_mostrar_categoria.sql
    Procedure para mostrar las categorias de un producto

--- sql/proc19_mostrar_categoria.sql
+++ sql/proc19_mostrar_categoria.sql
@@ -1,1 +1,8 @@
+DROP PROCEDURE IF EXISTS proc_mostrar_categoria;
 
+DELIMITER //
+CREATE PROCEDURE proc_mostrar_categoria()
+BEGIN
+	SELECT Id_Categoria, Nombre_Categoria FROM Categoria;
+END //
+DELIMITER ;

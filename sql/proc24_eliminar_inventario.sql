2b906ef81c86516c88dda482fe161913dbdc464c
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-06 19:46:42 -0600

    Update proc24_eliminar_inventario.sql
    Procedure para eliminar un producto del inventario

--- sql/proc24_eliminar_inventario.sql
+++ sql/proc24_eliminar_inventario.sql
@@ -1,1 +1,10 @@
-
+DROP PROCEDURE IF EXISTS proc_eliminar_inventario;
+DELIMITER //
+CREATE PROCEDURE proc_eliminar_inventario(
+   IN p_codigo_barras VARCHAR(13)
+)
+BEGIN
+   UPDATE Producto SET Estatus = '0'
+      WHERE Codigo_Barras = p_codigo_barras;
+END //
+DELIMITER ;

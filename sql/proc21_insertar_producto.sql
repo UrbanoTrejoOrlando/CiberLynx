9e582c304e6c7a0a1c7261beda6af2471b8a9a61
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-06 19:38:33 -0600

    Update proc21_insertar_producto.sql
    Procedure para insertar un nuevo producto

--- sql/proc21_insertar_producto.sql
+++ sql/proc21_insertar_producto.sql
@@ -1,1 +1,20 @@
+DROP PROCEDURE IF EXISTS proc_insertar_producto;
 
+DELIMITER //
+CREATE PROCEDURE proc_insertar_producto(
+IN p_marca INT,
+IN p_categoria INT,
+IN p_unidad INT,
+IN p_nombre VARCHAR(30),
+IN p_precio DOUBLE,
+IN p_existencia INT,
+IN p_descripcion VARCHAR(40)
+)
+BEGIN 
+DECLARE p_codigo_barras VARCHAR(13);
+    SET p_codigo_barras = GenerarClaveCodigoBarras();
+
+INSERT INTO Producto (Codigo_Barras, Id_Marca, Id_Categoria, Id_Unidad, Nombre, Precio, Existencia, Descripcion) 
+VALUES (p_codigo_barras, p_marca, p_categoria, p_unidad, p_nombre, p_precio, p_existencia, p_descripcion);
+END //
+DELIMITER ;

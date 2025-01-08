cd54681d203fc2b99b3709bc45af7dae28a0657f
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-07 19:39:21 -0600

    Update proc33_insertar_dispositivo.sql
    Procedure para insertar un dispositivo

--- sql/proc33_insertar_dispositivo.sql
+++ sql/proc33_insertar_dispositivo.sql
@@ -1,1 +1,14 @@
-
+DROP PROCEDURE IF EXISTS proc_insertar_dispositivo;
+DELIMITER //
+CREATE PROCEDURE proc_insertar_dispositivo(
+    IN p_id_tipo INT,
+    IN p_direccion_mac VARCHAR(17)
+)
+BEGIN 
+    DECLARE p_clv_dispositivo VARCHAR(5);
+    SET p_clv_dispositivo = GenerarClaveAleatoria12();
+    
+    INSERT INTO Dispositivo (Clv_Dispositivo, Id_Tipo_Dispositivo, Direccion_Mac)
+    VALUES(p_clv_dispositivo, p_id_tipo, p_direccion_mac);
+END //
+DELIMITER ;

142266c92b831e98bcf89952d91cfddc8e6db3da
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-07 20:00:44 -0600

    Update proc39_mostrar_cliente.sql
    Procedure para mostrar informacion de un cliente

--- sql/proc39_mostrar_cliente.sql
+++ sql/proc39_mostrar_cliente.sql
@@ -1,1 +1,12 @@
+DROP PROCEDURE IF EXISTS mostrar_cliente;
 
+DELIMITER //
+CREATE PROCEDURE mostrar_cliente(
+    IN p_clv_cliente VARCHAR(5)
+)
+BEGIN
+    SELECT CONCAT(Nombre, ' ', Apellido_Paterno, ' ', Apellido_Materno) AS Nombre_Completo 
+    FROM Cliente 
+    WHERE Clv_Cliente = p_clv_cliente;
+END //
+DELIMITER ;

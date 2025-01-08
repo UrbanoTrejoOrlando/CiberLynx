d006a4571d69565cf51e40e1353cd6c865de3cc2
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-07 19:28:23 -0600

    Update proc29_actualizar_usuario.sql
    Procedure para actualizar informacion de un usuario

--- sql/proc29_actualizar_usuario.sql
+++ sql/proc29_actualizar_usuario.sql
@@ -1,1 +1,20 @@
-
+DROP PROCEDURE IF EXISTS actualizar_usuario;
+DELIMITER //
+CREATE PROCEDURE actualizar_usuario(
+    IN p_clv_usuario VARCHAR(5),
+    IN p_nombre VARCHAR(20),
+    IN p_apellidop VARCHAR(20),
+    IN p_apellidom VARCHAR(20),
+    IN p_usuario_nombre VARCHAR(10),
+    IN p_contrasenia VARCHAR(10)
+)
+BEGIN 
+    UPDATE Usuario SET
+        Nombre = p_nombre,
+        Apellido_Paterno = p_apellidop,
+        Apellido_Materno = p_apellidom,
+        Usuario_Nombre = p_usuario_nombre,
+        Contrasenia = p_contrasenia
+    WHERE Clv_Usuario = p_clv_usuario;
+END //
+DELIMITER ;

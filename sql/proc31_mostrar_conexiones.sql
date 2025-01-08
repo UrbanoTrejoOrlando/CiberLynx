f5d2ce7d1ba9dac6cffa622a236893ae686d5e84
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-07 19:30:52 -0600

    Update proc31_mostrar_conexiones.sql
    Procedure para mostrar las conexiones de wifi

--- sql/proc31_mostrar_conexiones.sql
+++ sql/proc31_mostrar_conexiones.sql
@@ -1,1 +1,25 @@
+DROP PROCEDURE IF EXISTS proc_mostrar_conexiones;
+DELIMITER //
 
+CREATE PROCEDURE proc_mostrar_conexiones()
+BEGIN
+    SELECT
+        Conexion.clv_conexion,
+        Dispositivo.Direccion_Mac,
+        CASE Conexion.Estatus
+            WHEN 1 THEN 'Habilitado'
+            ELSE 'Inhabilitado'
+        END AS Estatus,
+        Producto.Nombre,
+        Conexion.Fecha_Inicio,
+        Conexion.Fecha_Final
+    FROM 
+        Conexion
+    INNER JOIN 
+        Dispositivo ON Conexion.Clv_Dispositivo = Dispositivo.Clv_Dispositivo
+    INNER JOIN 
+        Producto ON Conexion.Codigo_Barras = Producto.Codigo_Barras
+    WHERE 
+        Conexion.Estatus = 1; 
+END //
+DELIMITER ;

7f8ed437eda7d6a2ee4d21edc23af000d14d2ebd
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-07 19:40:44 -0600

    Update proc34_direccion_mac.sql
    Procedure para mostrar una direccion mac

--- sql/proc34_direccion_mac.sql
+++ sql/proc34_direccion_mac.sql
@@ -1,1 +1,9 @@
+DROP PROCEDURE IF EXISTS proc_direccion_mac;
+DELIMITER //
 
+CREATE PROCEDURE proc_direccion_mac()
+BEGIN
+        SELECT Clv_Dispositivo, Direccion_Mac
+        FROM Dispositivo;
+END //
+DELIMITER ;

19ab5695548afe6c4f19f734024212284d7b1254
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-07 20:04:53 -0600

    Update proc41_detalles_productos.sql
    Procedure para mostrar información sobre algun producto

--- sql/proc41_detalles_productos.sql
+++ sql/proc41_detalles_productos.sql
@@ -1,1 +1,27 @@
-
+DROP PROCEDURE IF EXISTS proc_detalles_productos;
+DELIMITER //
+CREATE PROCEDURE proc_detalles_productos(
+    IN fecha_inicio DATE,
+    IN fecha_fin DATE
+)
+BEGIN
+    SELECT 
+        Producto.Nombre,
+        Producto.Codigo_Barras,
+        SUM(Detalle_Venta.Cantidad) AS 'Cantidad',
+        Producto.Precio,
+        Categoria.Nombre_Categoria
+    FROM 
+        Detalle_Venta
+    INNER JOIN 
+        Venta ON Detalle_Venta.Folio_Venta = Venta.Folio_Venta
+    INNER JOIN 
+        Producto ON Detalle_Venta.Codigo_Barras = Producto.Codigo_Barras
+    INNER JOIN 
+        Categoria ON Producto.Id_Categoria = Categoria.Id_Categoria
+    WHERE 
+        DATE(Venta.Fecha) BETWEEN fecha_inicio AND fecha_fin
+    GROUP BY 
+        Producto.Nombre, Producto.Codigo_Barras, Producto.Precio, Categoria.Nombre_Categoria;
+END //
+DELIMITER ;

ff0397d3d49a59c897e7a6e9a9b8dfa816ed9db6
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-07 19:17:59 -0600

    Update proc28_consultar_usuario.sql
    Procedure para consultar un usuario en el sistema

--- sql/proc28_consultar_usuario.sql
+++ sql/proc28_consultar_usuario.sql
@@ -1,1 +1,19 @@
+DROP PROCEDURE IF EXISTS proc_consultar_usuario;
 
+DELIMITER //
+CREATE PROCEDURE proc_consultar_usuario(
+    IN p_clv_usuario VARCHAR(5)
+)
+BEGIN
+    SELECT
+    Clv_Usuario, 
+    Rol.Nombre_Rol,
+    Nombre,
+    Apellido_Paterno,
+    Apellido_Materno,
+    Usuario_Nombre,
+    Contrasenia FROM Usuario
+    INNER JOIN Rol ON Usuario.Id_Rol = Rol.Id_Rol
+    WHERE Clv_Usuario = p_clv_usuario;
+END //
+DELIMITER ;

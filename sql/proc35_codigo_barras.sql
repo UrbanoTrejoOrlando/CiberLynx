035e0d69e9fde8c069e569e086e27bbc98f3e61c
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-07 19:42:35 -0600

    Update proc35_codigo_barras.sql
    Procedure para mostrar el nombre del producto mediante el codigo de barras

--- sql/proc35_codigo_barras.sql
+++ sql/proc35_codigo_barras.sql
@@ -1,1 +1,9 @@
+DROP PROCEDURE IF EXISTS proc_codigo_barras;
 
+DELIMITER //
+CREATE PROCEDURE proc_codigo_barras()
+BEGIN
+        SELECT Codigo_Barras, Nombre
+        FROM Producto WHERE Existencia = 1;
+END //
+DELIMITER ;

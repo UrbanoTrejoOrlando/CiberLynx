63880038dd13f050a933bbf55e86e9a7eda4317b
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-07 19:29:39 -0600

    Update proc30_eliminar_usuario.sql
    Procedure para eliminar un usuario

--- sql/proc30_eliminar_usuario.sql
+++ sql/proc30_eliminar_usuario.sql
@@ -1,1 +1,10 @@
-
+DROP PROCEDURE IF EXISTS proc_eliminar_usuario;
+DELIMITER //
+CREATE PROCEDURE proc_eliminar_usuario(
+    IN p_clv_usuario VARCHAR(5)
+)
+BEGIN
+    UPDATE Usuario SET Estatus = '0'
+        WHERE Clv_Usuario = p_clv_usuario;
+END //
+DELIMITER ;

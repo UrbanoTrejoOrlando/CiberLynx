dfc5575237b5fc92a0a7e98c4623a8ec4c329336
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-07 20:02:08 -0600

    Update proc40_total_vendido.sql
    Procedure para mostrar el total de la venta

--- sql/proc40_total_vendido.sql
+++ sql/proc40_total_vendido.sql
@@ -1,1 +1,25 @@
+DROP PROCEDURE IF EXISTS proc_total_vendido_por_categoria;
 
+DELIMITER //
+CREATE PROCEDURE proc_total_vendido_por_categoria(
+    IN fecha_inicio DATE,
+    IN fecha_fin DATE
+)
+BEGIN
+    SELECT 
+        Categoria.Nombre_Categoria AS Categoria,
+        SUM(Producto.Precio * Detalle_Venta.Cantidad) AS TotalVendido
+    FROM 
+        Detalle_Venta
+    INNER JOIN 
+        Producto ON Detalle_Venta.Codigo_Barras = Producto.Codigo_Barras
+    INNER JOIN 
+        Categoria ON Producto.Id_Categoria = Categoria.Id_Categoria
+    INNER JOIN 
+        Venta ON Detalle_Venta.Folio_Venta = Venta.Folio_Venta
+    WHERE 
+        DATE(Venta.Fecha) BETWEEN fecha_inicio AND fecha_fin
+    GROUP BY 
+        Categoria.Nombre_Categoria;
+END //
+DELIMITER ;

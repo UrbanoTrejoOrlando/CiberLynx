9be98a57306b7af6aacb0656154023265d831a7b
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-07 19:51:15 -0600

    Update proc38_mostrar_productos.sql
    Procedure para mostrar productos

--- sql/proc38_mostrar_productos.sql
+++ sql/proc38_mostrar_productos.sql
@@ -1,1 +1,8 @@
+DROP PROCEDURE IF EXISTS proc_mos_productos_version_2;
 
+DELIMITER //
+CREATE PROCEDURE proc_mos_productos_version_2()
+BEGIN
+	SELECT Codigo_Barras, Nombre, Descripcion, Precio FROM Producto;
+END //
+DELIMITER ;

1e3b87f9c080684f0dbfa46fee88492ee1b5d8ae
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-08 20:31:54 -0600

    Update actualizarUsuariobd.php
    Actualizar informacion de un usuario a la base de datos

--- actualizarUsuariobd.php
+++ actualizarUsuariobd.php
@@ -1,1 +1,22 @@
+<?php
+	include("conexion/conectar_base.php");
+	$clave = $_POST['txtClave'];
+	$rol = $_POST['txtRol'];
+	$nombre = $_POST['txtNombre'];
+	$apellidoPaterno = $_POST['txtApellidoPaterno'];
+	$apellidoMaterno = $_POST['txtApellidoMaterno'];
+    $usuario = $_POST['txtUsuarioNombre'];
+    $contrasenia = $_POST['txtContrasenia'];
+
+	echo "$clave | $usuario | $contrasenia | $rol";
+
+	$sql = "CALL actualizar_usuario('$clave','$nombre','$apellidoPaterno','$apellidoMaterno','$usuario','$contrasenia');";
+  	if(mysqli_query($conexion,$sql)){
+   	# redireccionar a consultar usuarios
+   	 header("location:usuario.php");
+    	exit();
+ 	}else{
+    	echo "Problemas al actualizar los datos, verifique de nuevo: " . mysqli_error($conexion);
+  }
+?>
 

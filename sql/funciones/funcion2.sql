35f7312ca3be7135fe81225b6b8225333a02d16c
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-08 20:04:06 -0600

    Update funcion2.sql
    Funcion para crear un codigo de barras de 13 digitos

--- sql/funciones/funcion2.sql
+++ sql/funciones/funcion2.sql
@@ -1,1 +1,21 @@
+DELIMITER //
+
+CREATE FUNCTION GenerarClaveCodigoBarras() 
+RETURNS VARCHAR(13) 
+BEGIN
+    DECLARE clave VARCHAR(13) DEFAULT ''; -- Almacena la clave generada
+    DECLARE caracteres VARCHAR(10) DEFAULT '0123456789'; -- Conjunto de caracteres permitidos (dígitos)
+    DECLARE i INT DEFAULT 1; -- Contador para el ciclo
+
+    -- Generar clave aleatoria de 13 caracteres
+    WHILE i <= 13 DO
+        SET clave = CONCAT(clave, SUBSTRING(caracteres, FLOOR(1 + RAND() * LENGTH(caracteres)), 1));
+        SET i = i + 1;
+    END WHILE;
+
+    RETURN clave; -- Retorna la clave generada
+END //
+//
+
+DELIMITER ;
 

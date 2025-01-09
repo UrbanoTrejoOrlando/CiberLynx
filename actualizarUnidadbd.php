83183dabdd7d879488bbf2e0067a62860f1c037f
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-08 20:29:21 -0600

    Update actualizarUnidadbd.php
    Actualizacion del nombre de la unidad en la base de datos

--- actualizarUnidadbd.php
+++ actualizarUnidadbd.php
@@ -1,1 +1,16 @@
+<?php
+	include("conexion/conectar_base.php");
+	$idUnidad = $_POST['txtIdUnidad'];
+	$unidad = $_POST['txtNombreUnidad'];
+
+	$sql = "CALL proc_actualizar_unidad($idUnidad,'$unidad');";
+  	if(mysqli_query($conexion,$sql)){
+   	# redireccionar a consultar personas
+   	 header("location:tablas.php");
+    	exit();
+ 	}else{
+    	echo "Problemas al ingresar los datos, verifique de nuevo: " . mysqli_error($conexion);
+  }
+?>
+
 

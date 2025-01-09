02fc258420f13cf5a0796946c3f253812e26b079
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-08 20:21:02 -0600

    Update actualizarMarcabd.php
    Actualizacion a la base de datos sobre el cambio del nombre del producto

--- actualizarMarcabd.php
+++ actualizarMarcabd.php
@@ -1,1 +1,18 @@
+<?php
+	include("conexion/conectar_base.php");
+	$idMarca = $_POST['txtIdMarca'];
+	$marca = $_POST['txtNombreMarca'];
+
+	echo "$marca";
+
+	$sql = "CALL proc_actualizar_marca($idMarca,'$marca');";
+  	if(mysqli_query($conexion,$sql)){
+   	# redireccionar a consultar personas
+   	 header("location:tablas.php");
+    	exit();
+ 	}else{
+    	echo "Problemas al ingresar los datos, verifique de nuevo: " . mysqli_error($conexion);
+  }
+?>
+
 

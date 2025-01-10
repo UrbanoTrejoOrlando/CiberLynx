78a239dde14dd869d330e7012b304aedb63b6820
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-09 19:14:16 -0600

    Update agregar_conexionbd.php
    Registrar una nueva conexion a la base de datos

--- agregar_conexionbd.php
+++ agregar_conexionbd.php
@@ -1,1 +1,22 @@
+<?php
+	include("conexion/conectar_base.php");
+	$direccion = $_POST['cmbDireccion'];	
+	$fecha = $_POST['txtFechaF'];	
+	$venta = $_POST['cmbVenta'];	
+	$conexiones = $_POST['cmbConexion'];	
+
+	echo "$direccion | $fecha | $venta | $conexiones ";
+ 
+   $sql = "CALL proc_agregar_conexion('$direccion','$conexiones','$venta','$fecha');";
+  	if(mysqli_query($conexion,$sql)){
+   #redireccionar a wifi
+   	header("location:wifi.php");
+    	exit();
+	}
+	else{
+   	echo "Problemas al registrar la nueva conexion, verifique de nuevo: " . mysqli_error($conexion);
+	}
+
+
+?>
 

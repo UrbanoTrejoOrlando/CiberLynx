b8eb29fc449f79d85f42098488262cb646cedbcd
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-09 19:20:32 -0600

    Update agregarClientebd.php
    Agregar un nuevo cliente a la base de datos

--- agregarClientebd.php
+++ agregarClientebd.php
@@ -1,1 +1,21 @@
+<?php
+	include("conexion/conectar_base.php");
+	$nombre = $_POST['txtNombre'];	
+	$apellidoP = $_POST['txtApellidoP'];	
+	$apellidoM = $_POST['txtApellidoM'];	
+		
+	echo "$nombre | $apellidoP | $apellidoM ";
+ 
+   $sql = "CALL proc_insertar_cliente('$nombre','$apellidoP','$apellidoM');";
+  	if(mysqli_query($conexion,$sql)){
+   #redireccionar a usuarios
+   	header("location:cliente.php");
+    	exit();
+	}
+	else{
+   	echo "Problemas al registrar un nuevo usuario, verifique de nuevo: " . mysqli_error($conexion);
+	}
+
+
+?>
 

8ab15cefdbdbcf1ffe6c33f2ad07e991323ab485
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-09 19:32:58 -0600

    Update agregarMarcabd.php
    Agregar una nueva marca a la base de datos

--- agregarMarcabd.php
+++ agregarMarcabd.php
@@ -1,1 +1,17 @@
+<?php
+	include("conexion/conectar_base.php");
+	$marca = $_POST['txtMarca'];
+
+	echo "$marca";
+
+	$sql = "CALL proc_insertar_marca('$marca');";
+  	if(mysqli_query($conexion,$sql)){
+   	# redireccionar a consultar personas
+   	 header("location:tablas.php");
+    	exit();
+ 	}else{
+    	echo "Problemas al ingresar los datos, verifique de nuevo: " . mysqli_error($conexion);
+  }
+?>
+
 

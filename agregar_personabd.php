2ef27f395af537991dc25987415768b937ee69bb
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-09 19:15:28 -0600

    Update agregar_personabd.php
    Registro de una nueva persona a la base de datos

--- agregar_personabd.php
+++ agregar_personabd.php
@@ -1,1 +1,21 @@
+<?php
+	include("conexion/conectar_base.php");
+	$nombre = $_POST['txtNombre'];	
+	$apellidoP = $_POST['txtApellidoP'];	
+	$apellidoM = $_POST['txtApellidoM'];	
+		
+	echo "$nombre | $apellidoP | $apellidoM ";
+ 
+   $sql = "CALL proc_insertar_persona('$nombre','$apellidoP','$apellidoM');";
+  	if(mysqli_query($conexion,$sql)){
+   #redireccionar a usuarios
+   	header("location:persona.php");
+    	exit();
+	}
+	else{
+   	echo "Problemas al registrar un nuevo usuario, verifique de nuevo: " . mysqli_error($conexion);
+	}
+
+
+?>
 

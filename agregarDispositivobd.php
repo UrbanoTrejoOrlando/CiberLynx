76e37692acab861b53f449fd3f482490b7c665f2
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-09 19:29:27 -0600

    Update agregarDispositivobd.php
    Agregar un nuevo dispositivo a la base de datos

--- agregarDispositivobd.php
+++ agregarDispositivobd.php
@@ -1,1 +1,18 @@
+<?php
+	include("conexion/conectar_base.php");
+	$direcccion = $_POST['txtDireccionMac'];
+   	$dispositivo = $_POST['cmbDispositivo'];
+
+   echo " $dispositivo | $direcccion";
+
+ 	$sql = "CALL proc_insertar_dispositivo($dispositivo,'$direcccion');";
+   	if(mysqli_query($conexion,$sql)){
+     	#redireccionar a inventario
+  			header("location:wifi.php");
+         exit();
+      }
+		else{
+      	echo "Problemas al registrar el dispositivo verifique de nuevo: " . mysqli_error($conexion);
+		}
+?>
 

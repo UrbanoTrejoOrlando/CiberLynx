9b37b947f8cc51af6455f2bc1da7397ffc74137f
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-10 19:37:41 -0600

    Update clientesbd.php
    Registro de un nuevo cliente a la base de datos

--- clientesbd.php
+++ clientesbd.php
@@ -1,1 +1,17 @@
+<?php
+	include("conexion/conectar_base.php");
+  	$nombre = $_POST['cmb_Nombre'];
+
+  	echo "$nombre";
+
+	$sql = "CALL proc_insertar_clientes($nombre);";
+  	if(mysqli_query($conexion,$sql)){
+   # redireccionar a consultar personas
+   	header("location:cliente.php");
+    	exit();
+ 	 }else{
+    echo "Problemas al registrar al cliente, verifique de nuevo: " . mysqli_error($conexion);
+  		}
+?>
+
 

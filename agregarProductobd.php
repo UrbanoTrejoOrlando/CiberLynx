cc10b2ebabd3e33a7419ed27e26ed5cbacf8d340
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-10 19:03:18 -0600

    Update agregarProductobd.php
    Registrar un producto nuevo a la base de datos

--- agregarProductobd.php
+++ agregarProductobd.php
@@ -1,1 +1,23 @@
+<?php
+	include("conexion/conectar_base.php");
+	$nombre = $_POST['txtNombre'];
+	$descripcion = $_POST['txtDescripcion'];
+	$precio = $_POST['txtPrecio'];
+	$marca = $_POST['cmbMarca'];
+	$categoria = $_POST['cmbCategoria'];
+	$unidad = $_POST['cmbUnidad'];
+	$existencia = $_POST['txtExistencia'];
+
+	echo " $marca | $categoria| $unidad | $nombre | $precio | $existencia | $descripcion ";
+
+	$sql = "CALL proc_insertar_producto($marca, $categoria, $unidad, '$nombre', $precio, $existencia, '$descripcion');";
+	if(mysqli_query($conexion,$sql)){
+   	# redireccionar a consultar personas
+   	 header("location:inventario.php");
+    	exit();
+  	}else{
+    echo "Problemas al registrar el producto, verifique de nuevo: " . mysqli_error($conexion);
+  }
+
+?>
 

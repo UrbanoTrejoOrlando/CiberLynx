93680efb78eba5f3e854b47d4cad65f2038f5f17
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-10 19:07:21 -0600

    Update agregarProveedorbd.php
    Agregar un nuevo proveedor a la base de datos

--- agregarProveedorbd.php
+++ agregarProveedorbd.php
@@ -1,1 +1,20 @@
+<?php
+	include("conexion/conectar_base.php");
+	$proveedor = $_POST['txtProv'];	
+	$telefono = $_POST['txtTelefono'];
+	$correo = $_POST['txtCorreo'];	
+	$direccion = $_POST['txtDireccion'];
+		 
+   	$sql = "CALL insertar_proveedor('$proveedor', '$telefono','$correo','$direccion');";
+  	if(mysqli_query($conexion,$sql)){
+   #redireccionar a proveedores
+   	header("location:proveedores.php");
+    	exit();
+	}
+	else{
+   	echo "Problemas al registrar el usuario, verifique de nuevo: " . mysqli_error($conexion);
+	}
+
+
+?>
 

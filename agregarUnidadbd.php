994d32bcc65f504a593662e63046fc0a1a13b68f
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-10 19:16:56 -0600

    Update agregarUnidadbd.php
    Registrar una unidad a la base de datos

--- agregarUnidadbd.php
+++ agregarUnidadbd.php
@@ -1,1 +1,17 @@
+<?php
+	include("conexion/conectar_base.php");
+	$unidad = $_POST['txtUnidad'];
+
+	echo "$unidad";
+
+	$sql = "CALL proc_insertar_unidad('$unidad');";
+  	if(mysqli_query($conexion,$sql)){
+   	# redireccionar a consultar personas
+   	 header("location:tablas.php");
+    	exit();
+ 	}else{
+    	echo "Problemas al ingresar los datos, verifique de nuevo: " . mysqli_error($conexion);
+  }
+?>
+
 

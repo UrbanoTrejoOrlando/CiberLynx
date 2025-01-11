1550b2cc4a16d02c1cf147db08cbc43cb8a003b0
Orlando Urbano Trejo <143353946+UrbanoTrejoOrlando@users.noreply.github.com>
2025-01-10 19:18:04 -0600

    Update agregarUsuariobd.php
    Registrar un usuario a la base de datos

--- agregarUsuariobd.php
+++ agregarUsuariobd.php
@@ -1,1 +1,22 @@
+<?php
+	include("conexion/conectar_base.php");
+	$rol = $_POST['cmbRol'];	
+	$nombre = $_POST['txtNombre'];
+	$apellidoP = $_POST['txtP'];	
+	$apellidoM = $_POST['txtM'];
+	$clv_usuario = $_POST['txtUsuario'];	
+	$contrasenia = $_POST['txtContrasenia'];	
+	 
+   	$sql = "CALL proc_inserta_usuario($rol,'$nombre', '$apellidoP','$apellidoM','$clv_usuario','$contrasenia');";
+  	if(mysqli_query($conexion,$sql)){
+   #redireccionar a usuarios
+   	header("location:usuario.php");
+    	exit();
+	}
+	else{
+   	echo "Problemas al registrar el usuario, verifique de nuevo: " . mysqli_error($conexion);
+	}
+
+
+?>
 

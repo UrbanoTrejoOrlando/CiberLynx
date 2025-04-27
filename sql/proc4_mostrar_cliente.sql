5cdfd4b13a13df8acc530a938aa4f4e8a5d9eb62
UrbanoTrejoOrlando <orlandourbanotrejo@gmail.com>
2025-04-26 18:50:20 -0600

    Modificacion al procedure

--- sql/proc4_mostrar_cliente.sql
+++ sql/proc4_mostrar_cliente.sql
@@ -1,9 +1,11 @@
+-- Verificar si el procedure existe
 DROP PROCEDURE IF EXISTS proc_mostrar_clientes_nuevo;
 
 DELIMITER //
 
 CREATE PROCEDURE proc_mostrar_clientes_nuevo()
 BEGIN
+--- Select sobre la tabla 
 SELECT 
 Clv_Cliente, Nombre, Apellido_Paterno, Apellido_Materno FROM Cliente WHERE Estatus = '1';
 END //
